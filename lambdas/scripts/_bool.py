d738c74a86f3b16629ffc91bb20611d0847c52cb
Kaczkowski Maciej <Maciej.Kaczkowski@orange.com>
2022-02-22 14:32:35 +0100

    bool.py passes

---
+++ lambdas/scripts/_bool.py
@@ -0,0 +1,18 @@
+# Basics
+IDENTITY = lambda boolean: boolean
+TRUE = lambda x: lambda y: x
+FALSE = lambda x: lambda y: y
+
+# Logical operators
+NOT = lambda boolean: boolean(FALSE)(TRUE)
+
+OR = lambda x: lambda y: x (TRUE) (y)
+NOR = lambda x: lambda y: NOT(x(TRUE)(y))
+
+XOR = lambda x: lambda y: x (y(FALSE)(TRUE)) (y(TRUE)(FALSE))
+XNOR = lambda x: lambda y: NOT(XOR(x)(y))
+
+AND = lambda x: lambda y: x (y) (FALSE)
+NAND = lambda x: lambda y: NOT(x(y)(FALSE))
+
+IMPLICATION = lambda x: lambda y: x (y(TRUE)(FALSE)) (TRUE)
--- lambdas/tests/test_bool.py
+++ lambdas/tests/test_bool.py
@@ -1,26 +1,6 @@
 import pytest
 
-from scripts import AND, OR, TRUE, FALSE, NOT, XOR
-
-
-@pytest.mark.parametrize('left, right, expected', [
-    (FALSE, FALSE, FALSE),
-    (FALSE, TRUE,  FALSE),
-    (TRUE,  FALSE, FALSE),
-    (TRUE,  TRUE,  TRUE),
-])
-def test_and(left, right, expected):
-    assert AND(left)(right) is expected
-
-
-@pytest.mark.parametrize('left, right, expected', [
-    (FALSE, FALSE, FALSE),
-    (FALSE, TRUE,  TRUE),
-    (TRUE,  FALSE, TRUE),
-    (TRUE,  TRUE,  TRUE),
-])
-def test_or(left, right, expected):
-    assert OR(left)(right) is expected
+from scripts import TRUE, FALSE, NOT, OR, NOR, XOR, XNOR, AND, NAND, IMPLICATION
 
 
 @pytest.mark.parametrize('given, expected', [
@@ -35,7 +15,67 @@ def test_not(given, expected):
     (FALSE, FALSE, FALSE),
     (FALSE, TRUE,  TRUE),
     (TRUE,  FALSE, TRUE),
+    (TRUE,  TRUE,  TRUE),
+])
+def test_or(left, right, expected):
+    assert OR(left)(right) is expected
+
+
+@pytest.mark.parametrize('left, right, expected', [
+    (FALSE, FALSE, TRUE),
+    (FALSE, TRUE,  FALSE),
+    (TRUE,  FALSE, FALSE),
+    (TRUE,  TRUE,  FALSE),
+])
+def test_nor(left, right, expected):
+    assert NOR(left)(right) is expected
+
+
+@pytest.mark.parametrize('left, right, expected', [
+    (FALSE, FALSE, FALSE),
+    (FALSE, TRUE,  TRUE),
+    (TRUE,  FALSE, TRUE),
     (TRUE,  TRUE,  FALSE),
 ])
 def test_xor(left, right, expected):
     assert XOR(left)(right) is expected
+
+
+@pytest.mark.parametrize('left, right, expected', [
+    (FALSE, FALSE, TRUE),
+    (FALSE, TRUE,  FALSE),
+    (TRUE,  FALSE, FALSE),
+    (TRUE,  TRUE,  TRUE),
+])
+def test_xnor(left, right, expected):
+    assert XNOR(left)(right) is expected
+
+
+@pytest.mark.parametrize('left, right, expected', [
+    (FALSE, FALSE, FALSE),
+    (FALSE, TRUE,  FALSE),
+    (TRUE,  FALSE, FALSE),
+    (TRUE,  TRUE,  TRUE),
+])
+def test_and(left, right, expected):
+    assert AND(left)(right) is expected
+
+
+@pytest.mark.parametrize('left, right, expected', [
+    (FALSE, FALSE, TRUE),
+    (FALSE, TRUE,  TRUE),
+    (TRUE,  FALSE, TRUE),
+    (TRUE,  TRUE,  FALSE),
+])
+def test_nand(left, right, expected):
+    assert NAND(left)(right) is expected
+
+
+@pytest.mark.parametrize('left, right, expected', [
+    (FALSE, FALSE, TRUE),
+    (FALSE, TRUE,  TRUE),
+    (TRUE,  FALSE, FALSE),
+    (TRUE,  TRUE,  TRUE),
+])
+def test_implication(left, right, expected):
+    assert IMPLICATION(left)(right) is expected
