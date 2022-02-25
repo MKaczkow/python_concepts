8e7ed27222a0029c28e3116ad6add276b0686ba1
Kaczkowski Maciej <Maciej.Kaczkowski@orange.com>
2022-02-25 11:46:57 +0100

    working on arithmetic and numerals

---
+++ lambdas/scripts/_natural.py
@@ -0,0 +1,24 @@
+from ._bool import AND, TRUE, FALSE, IDENTITY
+
+
+# arithmetic
+INC = lambda n: lambda a: lambda b: a(n(a)(b))
+ADD = lambda a: lambda b: a(INC)(b)
+MUL = lambda a: lambda b: lambda c: a(b(c))
+DEC = lambda n: lambda f: lambda x: n(lambda g: lambda h: h(g(f)))(lambda _: x)(IDENTITY)
+SUB = lambda a: lambda b: b(DEC)(a)
+POW = lambda a: lambda b: b(a)
+DIFF = lambda a: lambda b: ADD(SUB(a)(b))(SUB(b)(a))
+
+# numbers
+ZERO  = FALSE
+ONE   = IDENTITY
+TWO   = lambda a: lambda b: a(a(b))
+THREE = lambda a: lambda b: a(a(a(b)))
+FOUR  = INC(THREE)
+FIVE  = ADD(TWO)(THREE)
+SIX   = MUL(TWO)(THREE)
+SEVEN = INC(SIX)
+EIGHT = MUL(FOUR)(TWO)
+NINE  = POW(THREE)(TWO)
+TEN   = MUL(FIVE)(TWO)
