e27f51b16c02ce6bcafca678fbcbf1c086e9e453
Maciej Kaczkowski <Maciej.Kaczkowski.stud@pw.edu.pl>
2022-04-27 10:26:24 +0200

    added str vs repr from CMS short tutorial

---
+++ str_vs_repr/main.py
@@ -0,0 +1,22 @@
+import datetime
+import pytz
+
+
+a = [1, 2, 3, 4]
+b = 'test_string'
+c = datetime.datetime.utcnow().replace(tzinfo=pytz.UTC)
+d = str(c)
+
+# the goal of __repr__ is to be unambigous
+print(repr(a))
+print(repr(b))
+print(f'repr(c) {c}')
+print(f'repr(c) {d}')
+
+# the goal of __str__ is to be readable
+print(str(a))
+print(str(b))
+print(f'str(c) {c}')
+print(f'str(c) {d}')
+
+# altough, right now, they are pretty much the same...
