c8a31daf1d61c696a54de6b0a214de3cb9924ba6
Maciej Kaczkowski <maciej.kaczkowski.stud@pw.edu.pl>
2024-05-02 10:14:38 +0200

    about mypy

---
+++ mypy_example/main.py
@@ -0,0 +1,17 @@
+def greeting(name):
+    return "Helo " + name
+
+def static_greeting(name: str) -> str:
+    return "Helo " + name
+
+# def main():
+    # should fail but not get caught by mypy
+    # because it doesn't check dynamicly typed functions
+    # greeting(123)
+    # greeting(b"world")
+static_greeting("123")   # OK
+# static_greeting(123)   # NOK
+# static_greeting(b"world")   # NOK
+
+# if __name__ == "__main__":
+#     main()
